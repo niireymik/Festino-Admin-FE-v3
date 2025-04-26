bddf33f79d91613e10980af670c977d81a52be61
김민지 <alswlk0306@gmail.com>
2025-04-26 18:31:03 +0900

    chore: 프로젝트  초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-primary">
+      <h1 className="text-3xl font-bold text-white">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
