221caae00ea4e0ec9f51754801cd85c6abe3e132
김민지 <alswlk0306@gmail.com>
2025-04-27 06:09:41 +0900

    format: 람다식 컴포넌트 선언

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-function App() {
+const App : React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-primary">
       <h1 className="text-3xl font-bold text-white">Tailwind is working!</h1>
