df95388c87705b15b2111f778ec577899d6cc475
김민지 <alswlk0306@gmail.com>
2025-05-04 03:27:41 +0900

    style: 기본 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,37 @@ import MobileLayout from './layouts/MobileLayout';
 import OrderLayout from './layouts/OrderLayout';
 import BoothListPage from './pages/booths/BoothListPage';
 import OrderStatistics from './pages/orders/OrderStatisticsPage';
+import DefaultLayout from './layouts/DefaultLayout';
 
 const App : React.FC = () => {
+  const isMobile = /iPhone|Android/i.test(navigator.userAgent);
+
   return (
     <BrowserRouter>
       <Routes>
-        {/* Main */}
-        <Route path="/">
-          <Route element={<BoothListPage />} />
-        </Route>
+      {!isMobile ? (
+          <Route element={<DefaultLayout />}>
+            {/* Main */}
+            <Route path="/">
+              <Route element={<BoothListPage />} />
+            </Route>
 
-        {/* Order */}
-        <Route path="/order" element={<OrderLayout />}>
-          {/* <Route path="" element={<OrderRealTime />} /> */}
-          {/* <Route path="ready" element={<OrderReady />} /> */}
-          {/* <Route path="cooking" element={<OrderCooking />} /> */}
-          {/* <Route path="finish" element={<OrderFinish />} /> */}
-          {/* <Route path="cancel" element={<OrderCancel />} /> */}
-          <Route path="statistics" element={<OrderStatistics />} />
-        </Route>
+            {/* Order */}
+            <Route path="/order" element={<OrderLayout />}>
+              {/* <Route path="" element={<OrderRealTime />} /> */}
+              {/* <Route path="ready" element={<OrderReady />} /> */}
+              {/* <Route path="cooking" element={<OrderCooking />} /> */}
+              {/* <Route path="finish" element={<OrderFinish />} /> */}
+              {/* <Route path="cancel" element={<OrderCancel />} /> */}
+              <Route path="statistics" element={<OrderStatistics />} />
+            </Route>
+          </Route>
+        ) : (
+          // Mobile 
+          <Route path="/mobile" element={<MobileLayout />}>
 
-        {/* Mobile */}
-        <Route path="/mobile" element={<MobileLayout />}>
-          
-        </Route>
+          </Route>
+        )}
       </Routes>
     </BrowserRouter>
   );
