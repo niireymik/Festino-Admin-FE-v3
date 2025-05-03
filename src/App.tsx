1639d0aa5d7e2c40c078fb0f7fd6e913cebe862e
김민지 <alswlk0306@gmail.com>
2025-05-04 01:45:24 +0900

    chore: 부스 목록, 주문 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import MobileLayout from './layouts/MobileLayout';
 import OrderLayout from './layouts/OrderLayout';
+import BoothListPage from './pages/booths/BoothListPage';
+import OrderStatistics from './pages/orders/OrderStatisticsPage';
 
 const App : React.FC = () => {
   return (
@@ -9,12 +11,17 @@ const App : React.FC = () => {
       <Routes>
         {/* Main */}
         <Route path="/">
-
+          <Route element={<BoothListPage />} />
         </Route>
 
         {/* Order */}
         <Route path="/order" element={<OrderLayout />}>
-
+          {/* <Route path="" element={<OrderRealTime />} /> */}
+          {/* <Route path="/ready" element={<OrderReady />} /> */}
+          {/* <Route path="/cooking" element={<OrderCooking />} /> */}
+          {/* <Route path="/finish" element={<OrderFinish />} /> */}
+          {/* <Route path="/cancel" element={<OrderCancel />} /> */}
+          <Route path="/statistics" element={<OrderStatistics />} />
         </Route>
 
         {/* Mobile */}
