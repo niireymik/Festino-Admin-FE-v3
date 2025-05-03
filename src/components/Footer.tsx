7c28a42e0ad54b18bc4e426019d8f58d9f38581a
김민지 <alswlk0306@gmail.com>
2025-05-04 03:27:17 +0900

    style: footer 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer : React.FC = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center bg-primary-200-dark h-full gap-5">
+      <div className="text-white text-3xl">Festino</div>
+      <div className="text-secondary-300 text-base text-nowrap">Copyright ⓒ 2025 Festino All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
