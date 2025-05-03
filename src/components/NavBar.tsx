6278704d7c0017cdab351356e6c4a5ba6357802f
김민지 <alswlk0306@gmail.com>
2025-05-04 03:27:29 +0900

    style: 네비게이션바 추가

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FestinoLogo from "./icons/FestinoLogo";
+
+const NavBar: React.FC = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  // 활성 상태
+  const [boothActive, setBoothActive] = useState(false);
+  const [reserveActive, setReserveActive] = useState(false);
+  const [orderActive, setOrderActive] = useState(false);
+
+  // 로그인 화면으로 이동
+  const logout = () => {
+    navigate('/login');
+  };
+
+  const setActiveClass = (active: boolean) => {
+    return active ? "font-semibold text-primary-800" : "";
+  };
+
+  // URL 변화 시 active 상태 갱신
+  useEffect(() => {
+    setBoothActive(pathname === "/" || pathname.startsWith("/booth"));
+    setReserveActive(pathname.startsWith("/reserve"));
+    setOrderActive(pathname.startsWith("/order"));
+  }, [pathname]);
+
+  return (
+    <div className="bg-white w-full flex justify-between px-[40px] 2xl:px-[50px] items-center h-full shadow-primary select-none">
+      <div
+        className="text-3xl text-primary-800 font-semibold cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <FestinoLogo />
+      </div>
+
+      <div className="flex justify-end gap-4 md:gap-[50px] lg:gap-[70px] text-secondary-500 text-xs md:text-sm lg:text-[16px] font-medium">
+        <div
+          className={`hover:text-primary-800 hover:font-semibold cursor-pointer hidden sm:block ${setActiveClass(boothActive)}`}
+          onClick={() => navigate("/")}
+        >
+          부스
+        </div>
+        <div
+          className={`hover:text-primary-800 hover:font-semibold cursor-pointer hidden sm:block ${setActiveClass(reserveActive)}`}
+          onClick={() => navigate("/reserve")}
+        >
+          테이블링
+        </div>
+        <div
+          className={`hover:text-primary-800 hover:font-semibold cursor-pointer hidden sm:block ${setActiveClass(orderActive)}`}
+          onClick={() => navigate("/order")}
+        >
+          주문조회
+        </div>
+        <div
+          className="hover:text-primary-800 hover:font-semibold cursor-pointer hidden sm:block"
+          onClick={() => {
+            logout();
+            navigate("/login");
+          }}
+        >
+          로그아웃
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
