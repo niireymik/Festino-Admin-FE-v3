005b4e5d254906d03b8b5341ba9fe37540b36f06
김민지 <alswlk0306@gmail.com>
2025-05-04 02:24:54 +0900

    chore: 기본 path @로 설정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MobileHeader from "../components/headers/MobileHeader";
+import MobileHeader from "@/components/headers/MobileHeader";
 
 const MobileLayout : React.FC = () => {
   return (
