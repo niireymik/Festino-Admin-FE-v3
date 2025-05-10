050b5922ea2e7d6e395199c3159e77b1c881910c
김민지 <alswlk0306@gmail.com>
2025-05-10 22:56:53 +0900

    feat: 월 format 맞춰주는 util 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,7 @@ export const prettyPrice = (price: number) => {
   if (typeof price === 'string') price = parseInt(price);
   return price.toLocaleString('ko-KR') + '원';
 };
+
+export const formatMonth = (month: number) => {
+  return String(month).padStart(2, '0');
+}
