3aaa6f26c0904a89814d0f68ef9483332797ed52
김민지 <alswlk0306@gmail.com>
2025-05-11 02:17:32 +0900

    refactor: 통계 타입 파일

---
+++ src/types/orders/statistics.types.ts
@@ -0,0 +1,25 @@
+export interface BoothInfo {
+  boothId?: string;
+  adminName?: string;
+};
+
+export interface Statistic {
+  menuName: string;
+  menuPrice: number;
+  menuCount: number;
+  menuSale: number;
+};
+
+export interface OrderStatisticState {
+  menuSaleList: Statistic[];
+  totalSale: number;
+};
+
+export interface OrderStatisticsStore {
+  type: number;
+  setType: (type: number) => void;
+  allOrderStatistics: OrderStatisticState;
+  boothInfo: BoothInfo;
+  setBoothInfo: (booth: BoothInfo) => void;
+  getStatistics: (params: { boothId: string; date: number; type: string }) => Promise<void>;
+};
