6e1146940ce3fab0a55b33d298f588e52613a63a
Hyochan <hyochan02@gmail.com>
2025-05-10 15:19:59 +0900

    chore: UUID 검사 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,8 @@ export const prettyPrice = (price: number) => {
   if (typeof price === 'string') price = parseInt(price);
   return price.toLocaleString('ko-KR') + '원';
 };
+
+export const isUUID = (uuid:string) => {
+  const uuidRegex = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$');
+  return uuidRegex.test(uuid);
+};
