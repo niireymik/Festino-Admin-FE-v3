a2bf9285d85bb16c9408ed6bec0566af3144ca3b
김민지 <alswlk0306@gmail.com>
2025-05-12 03:26:33 +0900

    chore: 테스트 데이터 삭제

--- src/stores/orders/cancelOrder.ts
+++ src/stores/orders/cancelOrder.ts
@@ -3,34 +3,7 @@ import { api } from '@/utils/api';
 import { CancelOrderStore } from '@/types/orders/order.types';
 
 export const useCancelOrder = create<CancelOrderStore>((set) => ({
-  // cancelList: [],
-
-  // 테스트용
-  cancelList: [
-    {
-      "orderId": "c8560b57-4d03-4f16-b343-d31a989b80f9",
-      "orderNum": 436,
-      "tableNum": 14,
-      "userName": "이규빈",
-      "phoneNum": "01084803668",
-      "totalPrice": 26000,
-      "menuList": [
-        {
-          "menuId": "b84f1649-4550-41d7-b92c-dc2030442efb",
-          "menuName": "보쌈 & 막국수 세트",
-          "menuCount": 1,
-          "menuPrice": 20000
-        },
-        {
-          "menuId": "22888f00-9777-49b0-9299-4e2ec17a7527",
-          "menuName": "갯마을 해물라면",
-          "menuCount": 1,
-          "menuPrice": 6000
-        }
-      ],
-      "createAt": "2024-09-13T18:35:48.462649"
-    }
-  ],
+  cancelList: [],
 
   getCancelOrderList: async ({ boothId, date }) => {
     if (!boothId) return false;
--- src/stores/orders/depositOrder.ts
+++ src/stores/orders/depositOrder.ts
@@ -3,68 +3,7 @@ import { api } from '@/utils/api';
 import { DepositOrderStore } from '@/types/orders/order.types';
 
 export const useDepositOrder = create<DepositOrderStore>((set) => ({
-  // waitDepositList: [],
-
-  // 테스트용
-  waitDepositList: [
-    {
-      orderId: "10359a21-7f82-4f01-b256-c6fb9c66aeac",
-      orderNum: 545,
-      tableNum: 1,
-      userName: "이희연",
-      phoneNum: "01024033289",
-      totalPrice: 20000,
-      menuList: [
-        {
-          menuId: "b84f1649-4550-41d7-b92c-dc2030442efb",
-          menuName: "보쌈 & 막국수 세트",
-          menuCount: 1,
-          menuPrice: 20000
-        }
-      ],
-      createAt: "2024-09-26T00:08:07.627268"
-    },
-    {
-      orderId: "0af60d15-7d3e-4b96-8e1b-5fdd5e2ccf38",
-      orderNum: 587,
-      tableNum: 1,
-      userName: "ㄴㄴ",
-      phoneNum: "010-1111-1111",
-      totalPrice: 38000,
-      menuList: [
-        {
-          menuId: "e9413c19-3405-417c-bb36-8b6bc032d914",
-          menuName: "강할머니 보쌈",
-          menuPrice: 15000,
-          menuCount: 2
-        },
-        {
-          menuId: "0bfa5b44-4921-41d6-b774-1a9e2155554f",
-          menuName: "시원~한 할매 묵사발",
-          menuPrice: 8000,
-          menuCount: 1
-        }
-      ],
-      createAt: "2025-05-10T06:44:29.117377"
-    },
-    {
-      orderId: "8249b7de-cf2e-46f2-9c11-f37a11181117",
-      orderNum: 588,
-      tableNum: 1,
-      userName: "ㅇㅇㅇ",
-      phoneNum: "010-2222-2222",
-      totalPrice: 20000,
-      menuList: [
-        {
-          menuId: "b84f1649-4550-41d7-b92c-dc2030442efb",
-          menuName: "보쌈 & 막국수 세트",
-          menuPrice: 20000,
-          menuCount: 1
-        }
-      ],
-      createAt: "2025-05-11T04:00:29.124672"
-    }
-  ],  
+  waitDepositList: [],
 
   getWaitDepositOrderList: async ({ boothId, date }) => {
     if (!boothId) return false;
