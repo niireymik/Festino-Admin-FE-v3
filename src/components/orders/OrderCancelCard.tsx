f31257fcc8f3b5a4f092ddce10211983dfaf30ee
김민지 <alswlk0306@gmail.com>
2025-05-11 22:00:26 +0900

    style: 주문 카드 색상 변경

---
+++ src/components/orders/OrderCancelCard.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { useOrderPopup } from '@/stores/orders/orderPopup';
+import { useTableDetail } from '@/stores/booths/tableDetail';
+import { getHourandMinute, prettyMenuNum, prettyPhoneNumber, prettyPrice } from '@/utils/utils';
+import IconClock from '../icons/IconClock';
+
+interface DepositMenu {
+  menuName: string;
+  menuPrice: number;
+  menuCount: number;
+}
+
+interface OrderCancelCardProps {
+  orderId: string;
+  orderNum: number;
+  tableNum: number;
+  userName: string;
+  phoneNum: string;
+  totalPrice: number;
+  menuList: DepositMenu[];
+  createAt: string;
+}
+
+const OrderCancelCard: React.FC<OrderCancelCardProps> = (props) => {
+  const { openPopup } = useOrderPopup();
+  const { getCustomTableNum } = useTableDetail();
+
+  const handleClickCancelRestore = () => {
+    openPopup({
+      type: 'cancel',
+      selectOrderInfo: {
+        orderId: props.orderId,
+        orderNum: props.orderNum,
+        orderType: 'CANCEL',
+        phoneNum: props.phoneNum,
+        tableNum: props.tableNum,
+        totalPrice: props.totalPrice,
+        userName: props.userName,
+        createAt: props.createAt,
+      },
+      selectMenuInfoList: props.menuList,
+    });
+  };
+
+  return (
+    <div className="w-full min-w-[350px] h-[400px] flex flex-col justify-between outline outline-1 outline-secondary-500 rounded-3xl max-w-[500px]">
+      <div className="flex justify-between w-full h-[73px] items-center rounded-t-3xl border-b border-secondary-150 px-[28px] text-sm font-semibold bg-secondary-200 flex-wrap gap-x-2">
+        <div>No.{props.orderNum}</div>
+        <div>{getCustomTableNum(props.tableNum)}번</div>
+        <div>{props.userName}</div>
+        <div>{prettyPhoneNumber(props.phoneNum)}</div>
+        <div className="flex items-center gap-2">
+          <IconClock className="p-[1px]" />
+          <div className="font-medium">{getHourandMinute(props.createAt)}</div>
+        </div>
+      </div>
+
+      <div
+        className={`relative h-[353px] w-full overflow-y-auto ${props.menuList.length < 7 ? 'overflow-y-hidden' : ''}`}
+      >
+        <table className="w-full bg-white relative">
+          <thead>
+            <tr className="h-[38px] border-b border-secondary-300 text-sm">
+              <th className="text-start min-w-[230px] align-middle pl-[28px]">메뉴</th>
+              <th className="min-w-[30px] text-center align-middle">수량</th>
+              <th className="min-w-[80px] text-right align-middle pr-[28px]">가격</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.menuList.map((menu, menuIndex) => (
+              <tr
+                key={menuIndex}
+                className="h-[40px] border-b border-primary-150 last:border-none hover:bg-slate-50 text-sm"
+              >
+                <td className="text-start align-middle pl-[28px]">{menu.menuName}</td>
+                <td className="min-w-[30px] text-center align-middle">{prettyMenuNum(menu.menuCount)}</td>
+                <td className="min-w-[80px] text-right align-middle pr-[28px]">{prettyPrice(menu.menuPrice)}</td>
+              </tr>
+            ))}
+            {props.menuList.length < 7 &&
+              Array.from({ length: 7 - props.menuList.length }).map((_, idx) => (
+                <tr key={`empty-${idx}`}>
+                  <td colSpan={3} className="h-[43px]" />
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="flex justify-between items-center h-[73px] w-full rounded-b-3xl px-[28px] bg-secondary-200">
+        <div
+          className="flex justify-center items-center rounded-2xl w-[100px] h-9 bg-white shrink-0 text-secondary-700 font-semibold text-sm cursor-pointer"
+          onClick={handleClickCancelRestore}
+        >
+          주문 복구
+        </div>
+        <div className="font-bold text-md text-secondary-700">{prettyPrice(props.totalPrice)}</div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCancelCard;
---
+++ src/components/orders/OrderFinishCard.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { getHourandMinute, prettyMenuNum, prettyPhoneNumber, prettyPrice } from '@/utils/utils';
+import { useOrderPopup } from '@/stores/orders/orderPopup';
+import { useTableDetail } from '@/stores/booths/tableDetail';
+import { useBaseOrder } from '@/stores/orders/tableStatusOrder';
+import IconClock from '@/components/icons/IconClock';
+import IconRecipe from '@/components/icons/IconRecipe';
+import IconOrderCheck from '@/components/icons/IconOrderCheck';
+import { useLocation } from 'react-router-dom';
+
+interface Menu {
+  menuName: string;
+  menuPrice: number;
+  menuCount: number;
+}
+
+interface OrderFinishCardProps {
+  orderId: string;
+  orderNum: number;
+  tableNum: number;
+  totalPrice: number;
+  menuList: Menu[];
+  userName: string;
+  createAt: string;
+  phoneNum: string;
+  finishAt?: string;
+}
+
+const OrderFinishCard: React.FC<OrderFinishCardProps> = (props) => {
+  const { openPopup } = useOrderPopup();
+  const { getCustomTableNum } = useTableDetail();
+  const { pathname } = useLocation();
+
+  const handleClickOrderRestore = () => {
+    openPopup({
+      type: 'finish',
+      selectOrderInfo: {
+        orderId: props.orderId,
+        orderNum: props.orderNum,
+        orderType: 'FINISH',
+        phoneNum: props.phoneNum,
+        tableNum: props.tableNum,
+        totalPrice: props.totalPrice,
+        userName: props.userName,
+        createAt: props.createAt,
+        finishAt: props.finishAt,
+      },
+      selectMenuInfoList: props.menuList,
+    });
+  };
+
+  return (
+    <>
+      {pathname.includes('finish') ? (
+        <div className="w-full min-w-[350px] h-[400px] rounded-3xl flex flex-col justify-between outline outline-1 outline-success-800-light-70 max-w-[500px]">
+          <div className="flex justify-between w-full h-[73px] items-center px-[28px] text-sm font-semibold bg-success-50 flex-wrap gap-x-2 rounded-t-3xl border-b border-secondary-100">
+            <div>No.{props.orderNum}</div>
+            <div>{getCustomTableNum(props.tableNum)}번</div>
+            <div>{props.userName}</div>
+            <div className="text-sm">{prettyPhoneNumber(props.phoneNum)}</div>
+            <div className="flex items-center gap-2">
+              <IconClock className="p-[1px]" />
+              <div className="text-sm font-medium">{getHourandMinute(props.createAt)}</div>
+            </div>
+          </div>
+          <div className={`relative h-[353px] w-full overflow-y-auto ${props.menuList.length < 7 ? 'overflow-y-hidden' : ''}`}>
+            <table className="w-full bg-white relative">
+              <thead>
+                <tr className="h-[38px] border-b border-secondary-300 text-sm">
+                  <th className="text-start min-w-[230px] align-middle pl-[28px]">메뉴</th>
+                  <th className="min-w-[30px] text-center align-middle">수량</th>
+                  <th className="min-w-[80px] text-right align-middle pr-[28px]">가격</th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.menuList.map((menu, index) => (
+                  <tr key={index} className="h-[40px] border-b border-success-50 last:border-none hover:bg-slate-50 text-sm">
+                    <td className="text-start align-middle pl-[28px]">{menu.menuName}</td>
+                    <td className="min-w-[30px] text-center align-middle">{prettyMenuNum(menu.menuCount)}</td>
+                    <td className="min-w-[80px] text-right align-middle pr-[28px]">{prettyPrice(menu.menuPrice)}</td>
+                  </tr>
+                ))}
+                {props.menuList.length < 7 &&
+                  Array.from({ length: 7 - props.menuList.length }).map((_, idx) => (
+                    <tr key={`empty-${idx}`}>
+                      <td colSpan={3} className="h-[43px]" />
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-between items-center h-[73px] w-full rounded-b-3xl px-[28px] bg-success-50">
+            <div
+              className="flex justify-center items-center rounded-2xl w-[100px] h-9 bg-white shrink-0 text-success-800 font-semibold text-sm cursor-pointer"
+              onClick={handleClickOrderRestore}
+            >
+              주문 복구
+            </div>
+            <div className="font-bold text-md text-secondary-700-light">{prettyPrice(props.totalPrice)}</div>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full min-w-[350px] h-[92px] rounded-3xl flex justify-between items-center outline outline-1 outline-success-800-light-70 bg-success-50 px-6">
+          <div className="w-full flex flex-col justify-center text-secondary-700 font-medium gap-[11px]">
+            <div className="flex gap-[11px] items-center">
+              <div className="flex items-center gap-[2px]">
+                <IconRecipe />
+                <div className="text-sm">{getHourandMinute(props.createAt)}</div>
+              </div>
+              <div className="flex items-center gap-[2px]">
+                <IconOrderCheck />
+                <div className="text-sm">{getHourandMinute(props.finishAt ?? '')}</div>
+              </div>
+            </div>
+            <div className="flex text-sm items-center gap-[10px]">
+              <div className="pl-[5px]">{props.tableNum}번 테이블</div>
+              <div>{props.userName}</div>
+              <div>{prettyPrice(props.totalPrice)}</div>
+            </div>
+          </div>
+          <div
+            className="flex justify-center items-center rounded-2xl w-[100px] h-9 bg-white shrink-0 text-success-800 font-semibold text-sm cursor-pointer"
+            onClick={handleClickOrderRestore}
+          >
+            주문 복구
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default OrderFinishCard;
