47fdc198f2fff94cbb15edfe570000ec6376ceab
김민지 <alswlk0306@gmail.com>
2025-05-14 01:19:45 +0900

    chore: 탭 크기 조정

--- src/types/logins/user.types.ts
+++ src/types/logins/user.types.ts
@@ -1,20 +1,20 @@
 export interface UserStore {
-    userId: string
-    password: string
-    isAdmin: boolean
-    isValidate: boolean
-    userOwnBoothId: string
-    isError: boolean
-    errorMessage: string
+  userId: string
+  password: string
+  isAdmin: boolean
+  isValidate: boolean
+  userOwnBoothId: string
+  isError: boolean
+  errorMessage: string
 
-    setUserId: (id: string) => void
-    setPassword: (pw: string) => void
-    setIsError: (err: boolean) => void
-    setErrorMessage: (msg: string) => void
+  setUserId: (id: string) => void
+  setPassword: (pw: string) => void
+  setIsError: (err: boolean) => void
+  setErrorMessage: (msg: string) => void
 
-    login: () => Promise<boolean>
-    logout: () => Promise<void>
-    isUserValid: () => Promise<{ isAdmin: boolean; isValidate: boolean }>
-    getUserOwnBoothId: () => Promise<string>
-    isUserOwnBooth: (boothId: string) => Promise<boolean>
+  login: () => Promise<boolean>
+  logout: () => Promise<void>
+  isUserValid: () => Promise<{ isAdmin: boolean; isValidate: boolean }>
+  getUserOwnBoothId: () => Promise<string>
+  isUserOwnBooth: (boothId: string) => Promise<boolean>
 }
