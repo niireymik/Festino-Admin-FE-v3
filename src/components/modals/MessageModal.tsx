86b84566a705b66a95a0c985cae3c986a5889efd
김민지 <alswlk0306@gmail.com>
2025-05-17 03:38:20 +0900

    style: 메세지 모달 기능 구현

--- src/components/modals/MessageModal.tsx
+++ src/components/modals/MessageModal.tsx
@@ -10,12 +10,15 @@ const MessageModal: React.FC = () => {
   const { message, setMessage, sendMessage } = useMessageStore();
   const { closeModal } = useBaseModal();
 
+  if (!messageInfo || typeof messageInfo === 'string') return null;
+
   const handleInputMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
     let value = event.target.value;
     if (value.length > 45) {
       value = value.slice(0, 45);
     }
-    setMessage(value);
+
+    setMessage(event.target.value);
   };
 
   const handleClickSendButton = () => {
@@ -29,21 +32,23 @@ const MessageModal: React.FC = () => {
   return (
     <div className="min-w-[455px] w-[600px] h-fit bg-white rounded-2xl p-8 flex flex-col justify-between">
       <div className="flex flex-col w-full gap-[28px]">
+        {/* 제목 & 닫기 버튼 */}
         <div>
-          <div className="w-full flex justify-between items-center gap-5 shrink-0 font-semibold text-xl text-primary-900 h-9">
+          <div className="w-full flex justify-between items-center gap-5 shrink-0 font-semibold text-xl text-primary-800 h-9">
             <div className="w-[25px]" />
             문자 커스텀
-            <IconClose onClick={() => closeModal()} className="w-8 h-8 p-2 cursor-pointer hover:bg-gray-100 rounded-2xl" />
+            <IconClose onClick={closeModal} className="w-8 h-8 p-2 cursor-pointer hover:bg-gray-100 rounded-2xl" />
           </div>
           <div className="text-secondary-700-light font-medium text-center text-sm">
             보낼 메시지 내용을 45글자 이내로 입력해주세요.
           </div>
         </div>
 
+        {/* 예약자 정보 */}
         <div className="w-full flex flex-col gap-4">
           <div className="text-secondary-700-light text-md ml-4">예약자 정보</div>
           <div className="relative w-full rounded-xl">
-            <table className="w-full outline-primary-900-light outline outline-1 rounded-xl">
+            <table className="w-full outline-primary-800-light outline outline-1 rounded-xl">
               <thead className="text-secondary-900 h-[40px]">
                 <tr className="bg-primary-700-light text-sm">
                   <th className="rounded-tl-xl px-5">No.</th>
@@ -54,16 +59,23 @@ const MessageModal: React.FC = () => {
               </thead>
               <tbody>
                 <tr className="text-center h-[40px] text-sm">
-                  <td>{messageInfo?.reservationNum}</td>
-                  <td>{messageInfo?.userName}</td>
-                  <td>{messageInfo?.phoneNum}</td>
-                  <td>{prettyDate(messageInfo?.updateAt)}</td>
+                  <td><span>{messageInfo.reservationNum}</span></td>
+                  <td><span>{messageInfo.userName}</span></td>
+                  <td><span>{messageInfo.phoneNum}</span></td>
+                  <td>
+                    <span>
+                      {messageInfo.updateAt
+                        ? `${prettyDate(messageInfo.updateAt)} 20:30`
+                        : '-'}
+                    </span>
+                  </td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
 
+        {/* 메시지 입력 */}
         <div className="relative">
           <input
             type="text"
@@ -71,20 +83,21 @@ const MessageModal: React.FC = () => {
             onChange={handleInputMessage}
             value={message}
             maxLength={45}
-            className="w-full h-[45px] border-1 border-secondary-700 rounded-xl px-[17px] font-medium focus:border-primary-900 focus:outline-none focus:border-1 pr-[80px] text-sm"
+            className="w-full h-[45px] border-1 border-secondary-700 rounded-xl px-[17px] font-medium focus:border-primary-800 focus:outline-none focus:border-1 pr-[80px] text-sm"
           />
           <div className="absolute bottom-3 right-5 text-secondary-900-light text-sm">{message.length}/45</div>
         </div>
 
+        {/* 버튼 */}
         <div className="w-full flex justify-end items-center text-xl gap-3">
           <button
-            className="is-button is-outlined font-semibold w-[60px] h-[35px] rounded-xl text-sm flex items-center justify-center text-primary-900 bg-white cursor-pointer select-none"
+            className="is-button is-outlined font-semibold w-[60px] h-[35px] rounded-xl text-sm flex items-center justify-center text-primary-800 bg-white cursor-pointer select-none"
             onClick={closeModal}
           >
             취소
           </button>
           <button
-            className="is-button font-semibold w-[60px] h-[35px] rounded-xl text-sm flex items-center justify-center text-white bg-primary-900 cursor-pointer select-none"
+            className="is-button font-semibold w-[60px] h-[35px] rounded-xl text-sm flex items-center justify-center text-white bg-primary-800 cursor-pointer select-none"
             onClick={handleClickSendButton}
           >
             전송
