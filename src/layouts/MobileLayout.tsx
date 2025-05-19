9d8c5830f26bb2a843037783b08a330bd2451b94
niireymik <155354743+niireymik@users.noreply.github.com>
2025-05-19 11:46:22 +0900

    add workflow

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import MobileHeader from "@/components/headers/MobileHeader";
 
 const MobileLayout : React.FC = () => {
   return (
     <div className="min-w-[375px] max-w-[500px] bg-white min-h-screen shadow-xs mx-auto">
-      <MobileHeader />
       <Outlet />
     </div>
   );
